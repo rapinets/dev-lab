9b76c7daf4ca92b74122c2717faad658aea239de
artday <admin@artday.com.ua>
2019-11-09 21:16:50 +0200

    singleton usage example added

---
+++ patterns/singletone.php
@@ -0,0 +1,65 @@
+<?php /** @noinspection ALL */
+
+abstract class Singleton {
+
+    private static $instance = null;
+
+    public static function getInstance()
+    {
+        if ($a= self::$instance === null) {
+            self::$instance = new static(...func_get_args());
+        }
+
+        return self::$instance;
+    }
+
+    protected function __construct(){}
+
+    protected function __clone(){}
+}
+
+
+class Config extends Singleton {
+
+    private $config = [];
+
+    protected function __construct($a, $b)
+    {
+        var_dump($a, $b);
+        $this->config = [
+            'db' => [
+                'driver' => 'mysql',
+                'host' => 'localhost',
+                'user' => 'root',
+                'password' => ''
+            ],
+            'app' => [
+                'app_name' => 'Super Social site',
+                'author' => 'Rapinets',
+            ]
+        ];
+    }
+
+    public function get($prop)
+    {
+        $array = explode( '.', $prop);
+
+        return $this->config[$array[0]][$array[1]];
+    }
+
+    public function set($prop, $value)
+    {
+        $array = explode( '.', $prop);
+        $this->config[$array[0]][$array[1]] = $value;
+    }
+}
+
+/*
+ *  Somewhere in client's code
+ */
+$config = Config::getInstance(5, 10);
+//echo $config->get('app.app_name');
+
+
+
+$container->getConfig();
